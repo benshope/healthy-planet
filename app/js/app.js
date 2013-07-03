ea125eb8f1ae41dc96176cbd0ebcb7b48c07a04a
Ben Shope <nimajnebs@gmail.com>
2013-07-03 16:48:09 -0700

    Remove routing hash

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ui.bootstrap']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl});
-    $routeProvider.otherwise({redirectTo: '/'});
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.
+      when('/', {templateUrl: 'partials/home.html', controller: HomeCtrl}).
+      otherwise({redirectTo: '/'});
     $locationProvider.html5Mode(true);
-}])
+  }])
 .run(function($rootScope, $location, $anchorScroll, $routeParams) {
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
     $location.hash($routeParams.scrollTo);
