51568a9255fdd23e8957a37161c60d3113c67001
Ben Shope <nimajnebs@gmail.com>
2013-07-16 22:08:14 -0700

    Update the app

--- fabfile.py
+++ fabfile.py
@@ -1,26 +1,21 @@
 from fabric.api import local
 
 # Start the development server - $ fab run
-def run(mode="normal"):
-    """ start the local app server """
+def run():
     local("dev_appserver.py .")
 
 # Upload the app to App Engine - $ fab upload
-def upload(app_id="healthy-planet", version="2-2"):
-    """ upload the app """
+def upload():
     local("appcfg.py --oauth2 update .")
 
 # Save to github - $ fab save:m="First commit"
-def save(m="Fab-update the app"):
+def save(m="Update the app"):
     """ save the to github """
     local("git add .")
     local("git commit -a -m '{0}'".format(m))
     local("git push")
 
 # Save to GitHub and upload to App Engine - $ fab update:m="First commit"
-def update(m="Fab-update the app"):
-    """ save the to github """
-    local("git add .")
-    local("git commit -a -m '{0}'".format(m))
-    local("git push")
+def update(m="Update the app"):
+    save(m)
     local("appcfg.py --oauth2 update .")
